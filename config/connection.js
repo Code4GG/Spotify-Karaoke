a4eff1f19408576b73f509f804b53b90d825a448
code4GG <cavallaro.av@gmail.com>
2018-03-13 20:26:54 -0400

    localhosting sequelize db connection

--- config/connection.js
+++ config/connection.js
@@ -1,31 +1,52 @@
 
 
-if (!global.hasOwnProperty('db')) {
-  var Sequelize = require('sequelize')
-    , sequelize = null
+// if (!global.hasOwnProperty('db')) {
+//   var Sequelize = require('sequelize')
+//     , sequelize = null
 
-  if (process.env.JAWSDB_URL) {
-    // the application is executed on Heroku ... use the postgres database
-    sequelize = new Sequelize(process.env.JAWSDB_URL, {
-      dialect:  'mysql',
-      logging:  true //false
-    })
-  } else {
-    // the application is executed on the local machine ... use mysql
-    sequelize = new Sequelize('karaoke_db', 'root', null)
+//   if (process.env.JAWSDB_URL) {
+//     // the application is executed on Heroku ... use the postgres database
+//     sequelize = new Sequelize(process.env.JAWSDB_URL, {
+//       dialect:  'mysql',
+//       logging:  true //false
+//     })
+//   } else {
+//     // the application is executed on the local machine ... use mysql
+//     sequelize = new Sequelize('karaoke_db', 'root', null)
+//   }
+
+//   global.db = {
+//     Sequelize: Sequelize,
+//     sequelize: sequelize,
+//     User:      sequelize.import(__dirname + '/user') 
+//     // add your other models here
+//   }
+
+//   /*
+//     Associations can be defined here. E.g. like this:
+//     global.db.User.hasMany(global.db.SomethingElse)
+//   */
+// }
+
+// module.exports = global.db;
+
+// *********************************************************************************
+// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
+// *********************************************************************************
+
+// Dependencies
+const Sequelize = require("sequelize");
+
+// Creates mySQL connection using Sequelize
+const sequelize = new Sequelize("karaoke_db", "root", "root", {
+  host: "localhost",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
   }
+});
 
-  global.db = {
-    Sequelize: Sequelize,
-    sequelize: sequelize,
-    User:      sequelize.import(__dirname + '/user') 
-    // add your other models here
-  }
-
-  /*
-    Associations can be defined here. E.g. like this:
-    global.db.User.hasMany(global.db.SomethingElse)
-  */
-}
-
-module.exports = global.db
+// Exports the connection for other files to use
+module.exports = sequelize;
