c20cf615ed9bb50e03e6d4e5154939b8e837eafa
code4GG <cavallaro.av@gmail.com>
2018-03-24 13:11:21 -0400

    config

--- config/connection.js
+++ config/connection.js
@@ -1,40 +1,42 @@
 
 
-if (!global.hasOwnProperty('db')) {
-  var Sequelize = require('sequelize')
-    , sequelize = null
+// if (!global.hasOwnProperty('db')) {
+//   var Sequelize = require('sequelize')
+//     , sequelize = null
 
-  if (process.env.JAWSDB_URL) {
-    // the application is executed on Heroku ... use the postgres database
-    sequelize = new Sequelize(process.env.JAWSDB_URL, {
-      dialect:  'mysql',
-      logging:  true //false
-    })
-  } else {
-    // the application is executed on the local machine ... use mysql
-    sequelize = new Sequelize('karaoke_db', 'root', null)
-  }
+//   if (process.env.JAWSDB_URL) {
+//     // the application is executed on Heroku ... use the postgres database
+//     sequelize = new Sequelize(process.env.JAWSDB_URL, {
+//       dialect:  'mysql',
+//       port: match[4],
+//       host: match[3],
+//       logging:  true //false
+//     })
+//   } else {
+//     // the application is executed on the local machine ... use mysql
+//     sequelize = new Sequelize('karaoke_db', 'root', null);
+//   }
 
-  global.db = {
-    Sequelize: Sequelize,
-    sequelize: sequelize,
-    // users:   sequelize.import(__dirname + '/users');
-    admins:   sequelize.import(__dirname + '/admins');
-    admin_access: sequelize.import(__dirname + '/admin_access');
-    // searched_songs:   sequelize.import(__dirname + '/models/searched_songs');
-    // singers:   sequelize.import(__dirname + '/models/singers');
-    // itunes:   sequelize.import(__dirname + '/models/itunes');
+//   global.db = {
+//     Sequelize: Sequelize,
+//     sequelize: sequelize,
+//     // users:   sequelize.import(__dirname + '/users');
+//     admins:   sequelize.import(__dirname + '/admins');
+//     admin_access: sequelize.import(__dirname + '/admin_access');
+//     // searched_songs:   sequelize.import(__dirname + '/models/searched_songs');
+//     // singers:   sequelize.import(__dirname + '/models/singers');
+//     // itunes:   sequelize.import(__dirname + '/models/itunes');
 
-    // add your other models here
-  }
+//     // add your other models here
+//   }
 
-  /*
-    Associations can be defined here. E.g. like this:
-    global.db.User.hasMany(global.db.SomethingElse)
-  */
-}
+//   /*
+//     Associations can be defined here. E.g. like this:
+//     global.db.User.hasMany(global.db.SomethingElse)
+//   */
+// }
 
-module.exports = global.db;
+// module.exports = global.db;
 
 // *********************************************************************************
 // CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
