dd00d85a752c77cdd5e8fa5626637494ef37a192
code4GG <cavallaro.av@gmail.com>
2018-03-24 12:09:08 -0400

    connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,53 +1,53 @@
 
 
-// if (!global.hasOwnProperty('db')) {
-//   var Sequelize = require('sequelize')
-//     , sequelize = null
+if (!global.hasOwnProperty('db')) {
+  var Sequelize = require('sequelize')
+    , sequelize = null
 
-//   if (process.env.JAWSDB_URL) {
-//     // the application is executed on Heroku ... use the postgres database
-//     sequelize = new Sequelize(process.env.JAWSDB_URL, {
-//       dialect:  'mysql',
-//       logging:  true //false
-//     })
-//   } else {
-//     // the application is executed on the local machine ... use mysql
-//     sequelize = new Sequelize('karaoke_db', 'root', null)
-//   }
+  if (process.env.JAWSDB_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    sequelize = new Sequelize(process.env.JAWSDB_URL, {
+      dialect:  'mysql',
+      logging:  true //false
+    })
+  } else {
+    // the application is executed on the local machine ... use mysql
+    sequelize = new Sequelize('karaoke_db', 'root', null)
+  }
 
-//   global.db = {
-//     Sequelize: Sequelize,
-//     sequelize: sequelize,
-//     User:      sequelize.import(__dirname + '/user') 
-//     // add your other models here
-//   }
+  global.db = {
+    Sequelize: Sequelize,
+    sequelize: sequelize,
+    User:      sequelize.import(__dirname + '/user') 
+    // add your other models here
+  }
 
-//   /*
-//     Associations can be defined here. E.g. like this:
-//     global.db.User.hasMany(global.db.SomethingElse)
-//   */
-// }
+  /*
+    Associations can be defined here. E.g. like this:
+    global.db.User.hasMany(global.db.SomethingElse)
+  */
+}
 
-// module.exports = global.db;
+module.exports = global.db;
 
 // *********************************************************************************
 // CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
 // *********************************************************************************
 
 // Dependencies
-const Sequelize = require("sequelize");
+// const Sequelize = require("sequelize");
 
-// Creates mySQL connection using Sequelize
-const sequelize = new Sequelize("karaoke_db", "root", "root", {
-  host: "localhost",
-  dialect: "mysql",
-  port: 3306,
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+// // Creates mySQL connection using Sequelize
+// const sequelize = new Sequelize("karaoke_db", "root", "root", {
+//   host: "localhost",
+//   dialect: "mysql",
+//   port: 3306,
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     idle: 10000
+//   }
+// });
 
-// Exports the connection for other files to use
-module.exports = sequelize;
+// // Exports the connection for other files to use
+// module.exports = sequelize;
