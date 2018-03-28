5ff0bbde1c82dd0661d2d87efab7e1449f689b33
code4GG <cavallaro.av@gmail.com>
2018-03-28 17:53:57 -0400

    bcrypt added

--- controllers/karaoke_controller.js
+++ controllers/karaoke_controller.js
@@ -7,6 +7,8 @@ const admins = require("../models/admin.js");
 const searched_songs = require('../models/searched_songs.js');
 const exhbs = require('express-handlebars');
 const db = require('../models');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 //redirects to index on load
 router.get('/', function(req,res){
@@ -93,22 +95,48 @@ router.post('/api/ituneData', function(req,res){
 })
 //used when admins create an account
 router.post('/api/admins', function(req,res){
-	
 	const admins = req.body;
-	db.admins.create({
-		name: req.body.name,
-		email: req.body.email,
-		pass: req.body.pass,
-		admin_access: true
-	}).then(function(results){
-		res.end();
+		bcrypt.hash(req.body.pass, saltRounds,function(err, hash){
+			req.body.pass = hash;
+			db.admins.create({
+			name: req.body.name,
+			email: req.body.email,
+			pass: req.body.pass,
+			admin_access: true
+		}).then(function(results){
+			res.redirect('/admins');
+		}).catch(function(errr){
+			console.log(errr);
+			res.json(errr);
+		});
 	});
 });
+
+router.post('/api/login', function(req,res){
+	const loggedUser = req.body;
+	let reqPass = req.body.pass;
+	let reqEmail = req.body.email;
+	db.admins.findOne({
+		where: {
+			email: reqEmail
+		}
+	}).then(function(user){
+
+		bcrypt.compare(reqPass, db.admins.pass, function(err, result){
+			if (result){
+				res.redirect('/admin_access');
+			}
+		})
+	})
+})
+
+
+
 //used for when songs are searched
 router.post('/api/searched_songs', function(res,res){
 	const searched_songs = req.body
 	db.searched_songs.create({
-		song: req.body.song,
+		song: req.body.song_request,
 		song_played: false
 	});
 });
